ddf0be75fbdfeb1adb079bbe6b4ad692f70c310d
Efrain Coutiño <l22141208@queretaro.tecnm.mx>
2025-01-10 00:57:39 -0600

    Game loop

---
+++ main.py
@@ -0,0 +1,28 @@
+import sys
+
+import pygame
+
+pygame.init()
+dark_blue = (44,44,127)
+
+#Esto setea el tamaño de la ventana en 300 de ancho y 600 de alto
+screen = pygame.display.set_mode((300, 600))
+pygame.display.set_caption("Python Tetris")
+
+#Esto se utiliza para controlar la velocidad de actualización de tu juego.
+clock = pygame.Clock()
+
+while True:
+    #Esto controla los eventos del juego, estamos obteniendo todos los eventos que ocurren en el juego.
+    for event in pygame.event.get():
+        #Esto está evaluando si el evento es de tipo QUIT, si es así, cerrará la ventana.
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+    #Esto pinta la pantalla de color azul.
+    screen.fill(dark_blue)
+    #Esto actualiza la pantalla del juego.
+    pygame.display.update()
+    #Esto controla la velocidad de actualización de tu juego (en este caso 60 FPS).
+    clock.tick(60)
